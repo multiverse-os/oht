c5f05435422e371546f681f3e1341f47d1617585
x <lostinblue@tuta.io>
2016-05-04 13:20:37 +0200

    Migrating logic into an interface class that UI's can interact with. Once this class is finished, the console UI, the web UI, the cmd line UI can all be tied to these functions to help standardize interaction with oht.

--- oht/interface.go
+++ oht/interface.go
@@ -1,1 +1,56 @@
 package oht
+
+import ()
+
+type Interface struct {
+}
+
+// Application Information
+func (interf *Interface) Version() (version string) {
+}
+
+// CONFIG
+func (interf *Interface) DisplayConfig() (config string) {
+
+}
+
+func (interf *Interface) SetConfigOption(key string, value string) (successful bool) {
+
+}
+
+func (interf *Interface) UnsetConfigOption(key string) (successful bool) {
+
+}
+
+func (interf *Interface) WebUI(status bool) (successful bool) {
+
+}
+
+// NETWORK
+func (interf *Interface) ListPeers(peers []string) {
+
+}
+
+func (interf *Interface) PeerSuccessor(peer string) {
+
+}
+
+func (interf *Interface) PeerPredecessor(peer string) {
+
+}
+
+func (interf *Interface) PeerFTable(peers []string) {
+
+}
+
+func (interf *Interface) CreateRing(successful bool) {
+
+}
+
+func (interf *Interface) ConnectToRing(onionAddress string) (successful bool) {
+
+}
+
+func (interf *Interface) RingLookupPeerById(id string) (peer string) {
+
+}
