fd108bb9531c63edd937c7cc5ea9af0a47e1b486
x <lostinblue@tuta.io>
2016-05-06 05:57:26 +0200

    Known and trusted peers has been removed, known peers can be done through a database, possibly the same with trusted. If it turns out trusted peers are more convienent to maintain a json file.

--- oht/config.go
+++ oht/config.go
@@ -27,8 +27,6 @@ type Config struct {
 	TorWebUIPort       string
 	Locale             string
 	DevMode            bool
-	KnownNodesFile     string
-	TrustedNodesFile   string
 	DataDir            string
 	GenesisFile        string `json:",omitempty"`
 	LogFile            string
@@ -50,8 +48,6 @@ func InitializeConfig(torListenPort, torSocksPort, torControlPort, torWebUIPort 
 		TorWebUIPort:       "8080",
 		Locale:             "en",
 		DevMode:            false,
-		KnownNodesFile:     "known-nodes.json",
-		TrustedNodesFile:   "trusted-nodes.json",
 		LogFile:            "log.json",
 		LogVerbosity:       1,
 		Custom:             make(map[string]string),
@@ -161,58 +157,3 @@ func (config *Config) setConfigOption(key, value string) bool {
 		return true
 	}
 }
-
-// parseNodes parses a list of discovery node URLs loaded from a .json file.
-//func (cfg *Config) parseNodes(file string) []*discover.Node {
-//	// Short circuit if no node config is present
-//	path := filepath.Join(cfg.DataDir, file)
-//	if _, err := os.Stat(path); err != nil {
-//		return nil
-//	}
-//	// Load the nodes from the config file
-//	blob, err := ioutil.ReadFile(path)
-//	if err != nil {
-//		log.Println("Failed to access nodes: %v", err)
-//		return nil
-//	}
-//	nodelist := []string{}
-//	if err := json.Unmarshal(blob, &nodelist); err != nil {
-//		log.Println("Failed to load nodes: %v", err)
-//		return nil
-//	}
-//	// Interpret the list as a discovery node array
-//	var nodes []*discover.Node
-//	for _, url := range nodelist {
-//		if url == "" {
-//			continue
-//		}
-//		node, err := discover.ParseNode(url)
-//		if err != nil {
-//			log.Println("Node URL %s: %v\n", url, err)
-//			continue
-//		}
-//		nodes = append(nodes, node)
-//	}
-//	return nodes
-//}
-//
-//func (cfg *Config) nodeKey() (*ecdsa.PrivateKey, error) {
-//	// use explicit key from command line args if set
-//	if cfg.NodeKey != nil {
-//		return cfg.NodeKey, nil
-//	}
-//	// use persistent key if present
-//	keyfile := filepath.Join(cfg.DataDir, "nodekey")
-//	key, err := crypto.LoadECDSA(keyfile)
-//	if err == nil {
-//		return key, nil
-//	}
-//	// no persistent key, generate and store a new one
-//	if key, err = crypto.GenerateKey(); err != nil {
-//		return nil, fmt.Errorf("could not generate server key: %v", err)
-//	}
-//	if err := crypto.SaveECDSA(keyfile, key); err != nil {
-//		log.PrintLn("could not persist nodekey: ", err)
-//	}
-//	return key, nil
-//}
