aa03fa7bc75f3830ee78a0a67d11ed500388a315
lostinblue <billmurray@tutanota.de>
2016-05-14 17:40:53 +0200

    Update cli.go

--- ui/cli/cli.go
+++ ui/cli/cli.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with go-ethereum. If not, see <http://www.gnu.org/licenses/>.
 
-// geth is the official command-line client for Ethereum.
+// Using geth cli as a example to build the oht client, they have a good structure to work from
 package main
 
 import (
